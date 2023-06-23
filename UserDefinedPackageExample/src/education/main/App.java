e5052b945cfe0c1b729d2ad35aa7156ab88909e2
mcaldeira <miguelcaldeira@yahoo.com>
2023-06-23 10:54:17 +0000

    Updated 23june

---
+++ UserDefinedPackageExample/src/education/main/App.java
@@ -0,0 +1,12 @@
+package education.main;
+//import education.college.Attendance;
+public class App {
+
+	public static void main(String[] args) {
+		education.college.Attendance att1 = new education.college.Attendance();
+		att1.display();
+		education.school.Attendance att2 = new education.school.Attendance();
+		att2.display();
+	}
+
+}
